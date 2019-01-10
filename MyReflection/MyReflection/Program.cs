16bfb6463c1dc21ae060b8e933dea4ac261c7ad1
Every <Every_126@outlook.com>
2019-01-10 15:29:15 +0800

    add myrelection

---
+++ MyReflection/MyReflection/Program.cs
@@ -0,0 +1,25 @@
+﻿//using Study;
+using System;
+using System.Reflection;
+
+namespace MyReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SqlserverHelper sqlserverHelper = new SqlserverHelper();
+            //sqlserverHelper.Query();
+            Console.WriteLine("Hello World!");
+            
+
+            Assembly assembly= Assembly.Load("Study");
+            Module[] modules= assembly.GetModules();
+            foreach (var item in modules)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+        }
+    }
+}
---
+++ MyReflection/Study/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Study
+{
+    public class SqlserverHelper    
+    {
+        public SqlserverHelper()
+        {
+            Console.WriteLine("构造函数");
+        }
+        public void Query()
+        {
+            Console.WriteLine("sqlserverhelper.query");
+        }
+    }
+}
