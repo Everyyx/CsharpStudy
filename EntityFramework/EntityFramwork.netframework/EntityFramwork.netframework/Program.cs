f9ba99bf9ce5d90c8b7035505020038c9ef22f26
Every MR <Every_126@outlook.com>
2019-04-26 23:17:26 +0800

    use ADO.NET to creat a model with the connection between consoleAPP and SqlServer(local)

---
+++ EntityFramework/EntityFramwork.netframework/EntityFramwork.netframework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramwork.netframework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdventureWorks2012Entities adventureWorks2012EntitiesProxy = new AdventureWorks2012Entities();
+            foreach (Product product in adventureWorks2012EntitiesProxy.Products)
+            {
+                Console.WriteLine(product.Name.ToString());
+            }
+            Console.WriteLine("============================================");
+            Console.WriteLine(adventureWorks2012EntitiesProxy.Products.Count());
+        }
+    }
+}
