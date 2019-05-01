4dd0d975b462d72da1fc80b5fdf37ff6ce5bd1f9
Every MR <Every_126@outlook.com>
2019-05-01 11:41:19 +0800

    using EntityFrameworkcore-dbfirst

---
+++ EntityFrameworkcore/EntityFrameworkcore.netcore/Program.cs
@@ -0,0 +1,25 @@
+﻿using EntityFrameworkcore.netcore.Models;
+using System;
+using System.Linq;
+
+namespace EntityFrameworkcore.netcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello EntityFrameworkcore");
+            //PM控制台中的链接字符串：Scaffold-Dbcontext -Connection "Server=.;Database=AdventureWorks2012;uid=sa;pwd=an15222055120" Microsoft.EntityFrameworkcore.SqlServer -OutputDir "Models"
+            using (Models.AdventureWorks2012Context dbcontext=new Models.AdventureWorks2012Context())
+            {
+                var person = dbcontext.Person.Where(b => b.FirstName.Contains("ken")).ToList();
+                Console.WriteLine("First name          lastname");
+                foreach (Person p in person)
+                {
+
+                    Console.WriteLine(p.FirstName+"            "+p.LastName);
+                }
+            }
+        }
+    }
+}
